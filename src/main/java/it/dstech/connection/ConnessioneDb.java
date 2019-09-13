d2f6cfdfe73f3a9be964205fe5c2e3576fbe9c59
Fab91-r <fabiano.ruggeri91@gmail.com>
2019-09-13 13:10:15 +0200

    metodi Db e Controller

--- src/main/java/it/dstech/connection/ConnessioneDb.java
+++ src/main/java/it/dstech/connection/ConnessioneDb.java
@@ -2,7 +2,12 @@ package it.dstech.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnessioneDb {
 	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -14,5 +19,30 @@ public class ConnessioneDb {
 		Class.forName(JDBC_DRIVER);
 		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		return conn;
+	}
+
+	public static List<String> getContinenti() throws ClassNotFoundException, SQLException {
+		String query = "select distinct Continent from country;";
+		PreparedStatement statement = getConnessione().prepareStatement(query);
+		ResultSet result = statement.executeQuery();
+		List<String> listaContinenti = new ArrayList<String>();
+		while (result.next()) {
+			String continente = result.getString(1);
+			listaContinenti.add(continente);
+		}
+		return listaContinenti;
+	}
+	
+	public static List<String> getStati(String continente) throws ClassNotFoundException, SQLException {
+		String query = "SELECT Name from country where Continent = ?;";
+		PreparedStatement ps = getConnessione().prepareStatement(query);
+		ps.setString(1, continente);
+		ResultSet result = ps.executeQuery();
+		List<String> listaStati = new ArrayList<String>();
+		while (result.next()) {
+			String stato = result.getString(1);
+			listaStati.add(stato);
+		}
+		return listaStati;
 	}
 }
--- src/main/java/it/dstech/controller/WelcomeController.java
+++ src/main/java/it/dstech/controller/WelcomeController.java
@@ -1,8 +1,26 @@
 package it.dstech.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.dstech.connection.ConnessioneDb;
 
 @Controller
 public class WelcomeController {
 
+	
+	   @GetMapping("/")
+   public String main(Model model) throws ClassNotFoundException, SQLException {      
+
+    List <String> listaCont = ConnessioneDb.getContinenti();
+
+    model.addAttribute("listaCont", listaCont);
+     return "welcome"; 
+ }
 }
